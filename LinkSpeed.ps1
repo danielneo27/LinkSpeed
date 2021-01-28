aaa27a4c955cf1d913b537deb01a1a09d281fe66
Daniel Neo <78002139+danielneo27@users.noreply.github.com>
2021-01-28 16:40:28 -0300

    v0.3.0 upd1

---
+++ LinkSpeed.ps1
@@ -0,0 +1,23 @@
+﻿<#	
+	.NOTAS
+	===========================================================================
+	 Criado em:   	15/12/2020 14:34
+	 Criado por:   	Daniel Neo
+     Versão:        0.3.0
+     Atualizado:    28/01/2021
+	 Nome do arquivo:  LinkSpeed
+	===========================================================================
+	.DESCRIÇÃO
+	   Mostrar velocidade máxima de internet suportada pela porta de rede do seu dispositivo.
+#>
+cls
+$host.UI.RawUI.ForegroundColor = "DarkGreen"
+$host.UI.RawUI.BackgroundColor = "Black"
+Unblock-File -Path C:\Users\$env:UserName\Downloads\LinkSpeed.ps1
+cls
+
+Get-NetAdapter
+
+Get-NetAdapter | where Status -eq "Up" | select InterfaceDescription, LinkSpeed
+
+Read-Host -Prompt "Pressione Enter para sair"
